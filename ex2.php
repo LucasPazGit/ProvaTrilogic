ae3465c64ad5f61d9f5da3ad2102b77da4dce88a
LucasPazGit <164208783+LucasPazGit@users.noreply.github.com>
2024-03-20 12:29:10 -0300

    Ex2.php
    Exercicio 2 da Prova do Trilogic2024

---
+++ ex2.php
@@ -0,0 +1,38 @@
+<?php
+
+function comparaIdade(){
+
+return $a['idade'] - $b['idade'];
+}
+
+
+$users = array(
+
+  array('nome' => 'Jorge Figueiredo', 'idade' => 29),
+  array('nome' => 'Amanda de Souza', 'idade' => 24),
+  array('nome' => 'Francisco Ferdinando', 'idade' => 28),
+  array('nome' => 'Albuquerque', 'idade' => 36),
+  array('nome' => 'Rosalinda Álvarez', 'idade' => 24)
+
+);
+
+usort($usuarios, 'comparaIdade');
+
+
+foreach ($usuarios as $usuario) {
+  
+  echo $usuario['nome'] . ' - ' . $usuario['idade'] . ' anos' . "<br>";
+}
+
+
+?>
+
+/*
+nasc
+Jorge 1995
+Amanda 2000
+Francisco 1996
+Andressa 1988
+Rosalinda 2000
+
+*/
